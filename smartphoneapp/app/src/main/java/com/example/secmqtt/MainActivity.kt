27fb8236c07c847a265b11f2bc2c51eafda05f19
Alessandro Di Vincenzo <aledivi96@gmail.com>
2021-11-28 16:51:00 +0100

    add smartphone application folder

---
+++ smartphoneapp/app/src/main/java/com/example/secmqtt/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.secmqtt
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.homebutton)
+        button.setOnClickListener {
+            val intent = Intent(this, SecondaryActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
