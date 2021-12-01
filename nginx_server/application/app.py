fdd87f9ec804254efe649e5cde3236eaaec292bc
Alessandro Di Vincenzo <aledivi96@gmail.com>
2021-12-01 22:31:44 +0100

    handle request implementation

--- nginx_server/application/app.py
+++ nginx_server/application/app.py
@@ -1,19 +1,19 @@
 from flask import Flask
 from flask import send_file
+from flask import request
+from datetime import datetime
 
 app = Flask(__name__)
-@app.route("/1")
-def send1():
-    return send_file("./C2_1")
 
-@app.route("/2")
-def send2():
-    return send_file("./C2_2")
-
-@app.route("/3")
-def send3():
-    return send_file("./C2_3")
-
+@app.route("/<number>", methods = ['POST'])
+def handle_n_serve(number):
+    if request.method == 'POST':
+        content = request.get_json()
+        date_time = content["timestamp"]
+        date_time_req = datetime.strptime(date_time, '%d/%m/%y %H:%M:%S')
+        now = datetime.now()
+        if (now - date_time_req).total_seconds() <= 60: #to avoid replay attack
+            return send_file("./C2_" + number, as_attachment=True)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
