5c345cc96725f2819428a35b40e8973153ac96f9
Alessandro Di Vincenzo <aledivi96@gmail.com>
2022-01-25 18:36:15 +0100

    implement MQTT publisher

---
+++ IoT_device/IoTpub.py
@@ -0,0 +1,197 @@
+import paho.mqtt.client as mqtt
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+from smartcard.System import readers
+from smartcard.util import toHexString, toASCIIString, toBytes
+import time
+import sys
+
+def generateFinalKnowledge(k1, k2):
+    master_key = bytes(b1 ^ b2 for (b1, b2) in zip(k1, k2)) # to have completion of final knowledge
+    return master_key
+
+def acquireK1():
+    f = open(sys.argv[1], "rb")
+    k1 = f.read()
+    f.close()
+    return k1
+
+def waitCard(connection):
+    try:
+        connection.connect()
+        return True
+    except:
+        print("Waiting smartcard")
+        return False
+
+def readCard(connection):
+    end = False
+    i = 1 # start from sector 1
+    typefield = ""  
+    payload = ""
+    while not end:
+    # decrypt first block of sector with key. if succeed, sector is unlocked
+    # if other sector is unlocked, previous sector is locked
+        COMMAND = [0xFF, 0x86, 0x00, 0x00, 0x05, 0x01, 0x00, i*4, 0x60, 0x00]
+        data, sw1, sw2 = connection.transmit(COMMAND)
+        if (sw1, sw2) == (0x90, 0x0):
+            print("Status: Decryption sector "+ str(i) +" using key #0 as Key A successful.")
+        elif (sw1, sw2) == (0x63, 0x0):
+            print("Status: Decryption sector "+ str(i) +" failed. Trying as Key B")
+            COMMAND = [0xFF, 0x86, 0x00, 0x00, 0x05, 0x01, 0x00, i*4, 0x61, 0x00]
+            data, sw1, sw2 = connection.transmit(COMMAND)
+        if (sw1, sw2) == (0x90, 0x0):
+            print("Status: Decryption sector "+ str(i) +" using key #0 as Key B successful.")
+        elif (sw1, sw2) == (0x63, 0x0):
+            print("Status: Decryption sector "+ str(i) +" failed.")
+            sys.exit()
+	
+        print("---------------------------------Sector "+ str(i) +"---------------------------------")
+        for block in range(i*4, i*4+3): # because last block of each sector is the key
+            COMMAND = [0xFF, 0xB0, 0x00]
+            COMMAND.append(block)
+            COMMAND.append(16)    
+            data, sw1, sw2 = connection.transmit(COMMAND)
+            blockdata = toHexString(data).replace(" ","")
+            if(block==4): # first block
+
+                ndefFormat = False
+                j = 1
+                while(not ndefFormat):
+                    if(blockdata[2*(j-1):2*j]!="03"):
+                        if(blockdata[2*(j-1):2*j]=="00"):
+                            print("Ignore byte #" + str(j))
+                            j = j + 1
+                        else:
+                            print("NO ndef format found")
+                            sys.exit()
+                    else:
+                        ndefFormat = True
+                
+                length = blockdata[j*2:(j+1)*2]
+                if(length=="00"):
+                    print("NO value")
+                    sys.exit()
+                if(length=="FF"):
+                    j = j + 1
+                    length = int(blockdata[j*2:(j+2)*2],16)
+                else:
+                    length = int(blockdata[j*2:(j+1)*2],16)
+                print("Total length = " + str(length))
+                
+                j=j+2
+                typelength = blockdata[j*2:(j+1)*2]
+                if(typelength=="00"):
+                    print("NO type field")
+                if(typelength=="FF"):
+                    j = j + 1
+                    typelength = int(blockdata[j*2:(j+2)*2],16)
+                else:
+                    typelength = int(blockdata[j*2:(j+1)*2],16)
+                print("Type length = " + str(typelength))
+
+                j = j + 1
+                payloadlength = blockdata[j*2:(j+1)*2]
+                if(payloadlength=="FF"):
+                    j = j + 1
+                    payloadlength = int(blockdata[j*2:(j+2)*2],16)
+                else:
+                    payloadlength = int(blockdata[j*2:(j+1)*2],16)
+                print("Payload length = " + str(payloadlength))
+                
+                j = j + 1
+                if(j<16):
+                    if(typelength+j>=16):
+                        typefield = typefield + blockdata[j*2:]
+                    else:
+                        typefield = typefield + blockdata[j*2:j*2+typelength*2]
+                        payload = payload + blockdata[j*2+typelength*2:]
+                        payloadlength = payloadlength - (16 - j - typelength)
+                    typelength = typelength - (16 - j)
+                    print("Type length = " + str(typelength))
+            else:
+                print("Payload length = " + str(payloadlength))
+                if(typelength>0): # not first block but typefield to read
+                    if(typelength<=16):
+                        if(typelength==16):
+                            typefield = typefield + blockdata
+                        else:
+                            typefield = typefield + blockdata[:typelength*2]
+                            payload = payload + blockdata[typelength*2:]
+                            payloadlength = payloadlength - (16-typelength)
+                        typelength = 0
+                    else:
+                        typefield = typefield + blockdata
+                        typelength -= 16
+                else:
+                    if(payloadlength<=16):
+                        payload += blockdata[:payloadlength*2] 
+                        payloadlength = 0
+                        end = True
+                    else:
+                        payload += blockdata
+                        payloadlength -= 16
+
+            print("block "+ str(block) +":\t"+ toHexString(data) +" | "+''.join(chr(j) for j in data))
+
+        print("Status words: %02X %02X" % (sw1, sw2))
+        if (sw1, sw2) == (0x90, 0x0):
+            print("Status: The operation completed successfully.")
+        elif (sw1, sw2) == (0x63, 0x0):
+            print("Status: The operation failed. Maybe auth is needed.")
+    
+        i = i+1 
+
+    typestring = toASCIIString(toBytes(typefield))
+    print(payload)
+    k2 = bytearray.fromhex(payload)
+    return k2, typestring
+
+k1 = acquireK1()
+
+r = readers()
+if len(r) < 1:
+	print("error: No readers available!")
+	sys.exit()
+
+print("Available readers: ", r)
+
+reader = r[0]
+print("Using: ", reader)
+
+connection = reader.createConnection()
+while(True):
+    if(waitCard(connection)):
+        print("Smartcard revealed")
+        break
+    else:
+        time.sleep(5)
+        continue
+
+k2, typestring = readCard(connection)
+print("Type: " + typestring)
+print(k2)
+
+master_key = generateFinalKnowledge(k1, k2)
+
+def on_publish(client,userdata,result):             #create function for callback
+    print("data published\n")
+    pass
+
+#broker_address="192.168.1.184" 
+broker_address="broker.hivemq.com" #use external broker
+print("creating new publisher instance")
+client = mqtt.Client("braintech_publisher") #create new instance
+client.on_publish = on_publish
+print("connecting to broker")
+client.connect(broker_address) #connect to broker
+
+message = "This is a test message from publisher"
+cipher = AES.new(master_key, AES.MODE_ECB)
+paddedtext = pad(message.encode("utf-8"), AES.block_size)
+print(paddedtext)
+message = cipher.encrypt(paddedtext)
+print(message)
+byteArr = bytearray(message)
+print("Publishing to topic","braintech/tesi/test1")
+client.publish("braintech/tesi/test1", byteArr) #publish
